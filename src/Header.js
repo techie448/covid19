030e135ee17fc2d44f3e6186ca37233648e9c1f8
rishabhkharbanda <techie448@gmail.com>
2020-05-02 09:26:26 -0400

    :dizzy: :art: Animations and design for welcome scrren. :sparkles: Info widgets

--- src/Header.js
+++ src/Header.js
@@ -1,11 +1,15 @@
 import React from 'react';
 import Info from "./info";
+import {ReactComponent as Bat} from "./bat.svg";
 
 function Header({count}) {
     return (
         <header>
-            <a href="https://github.com/techie448/covid19"></a>
-            <span>COVID-19 Dashboard</span>
+            <a href="https://github.com/techie448/covid19" class='icon'></a>
+            <div className='heading'>
+                <Bat className="bat"/>
+                COVID-19 Dashboard
+            </div>
             <Info count={count}/>
 
         </header>
--- src/info.js
+++ src/info.js
@@ -1,12 +1,37 @@
 import React from 'react';
+import {ReactComponent as Confirmed} from "./virus.svg";
+import {ReactComponent as Fight} from "./fight.svg";
+import {ReactComponent as Recovered} from "./medical.svg";
+import {ReactComponent as Death} from "./death.svg";
+import CountUp from 'react-countup';
 
 function Info({endDate, count}) {
     return (
-        <div> emojiss
-            total cases : {count.confirmed}
-            recovered : {count.recovered}
-            deaths : {count.deaths}
-            currently infected : {count.confirmed - count.recovered - count.deaths}
+        <div class="info">
+            <div className='c out'>
+                <Confirmed/>
+                <p><CountUp end={count.confirmed}/></p>
+                <p>Tested Positive</p>
+                <div class='c box'></div>
+            </div
+            >
+            <div className='r out'><Recovered/> <p><CountUp end={count.recovered}/></p> <p> Recovered</p>
+                <div className='r box'></div>
+            </div>
+
+            <div className='d out'><Death/> <p><CountUp end={count.deaths}/></p> <p> Deaths</p>
+                <div className='d box'></div>
+            </div>
+
+            <div className='c out'><Fight/><p><CountUp end={count.confirmed - count.recovered - count.deaths}/></p>
+                <p>Fighting</p>
+
+                <div className='box f'>
+                    <div className='box r'></div>
+                    <div className='box c'></div>
+                    <div className='box d'></div>
+                </div>
+            </div>
         </div>
     );
 }
