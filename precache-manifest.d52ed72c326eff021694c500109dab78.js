ff18b137f7c947520ff1387be364dd286752bc18
techie448 <techie448@gmail.com>
2020-07-12 02:27:27 -0400

    Updates

---
+++ precache-manifest.d52ed72c326eff021694c500109dab78.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "30f8211ac4dbd572639c845f3096a904",
+    "url": "./index.html"
+  },
+  {
+    "revision": "472359e31289d486a47a",
+    "url": "./static/css/main.f75c0d3a.chunk.css"
+  },
+  {
+    "revision": "cfd920fe9cf2ba1a6c54",
+    "url": "./static/js/2.dc7d7841.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "./static/js/2.dc7d7841.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "472359e31289d486a47a",
+    "url": "./static/js/main.2fad98f2.chunk.js"
+  },
+  {
+    "revision": "0d2373d6ed7feee294c2",
+    "url": "./static/js/runtime-main.9f834337.js"
+  },
+  {
+    "revision": "fff1a8d54f8435dbcd26c927cfb954e3",
+    "url": "./static/media/bat.fff1a8d5.svg"
+  },
+  {
+    "revision": "73d51985291075686c93f7fe5cf129e8",
+    "url": "./static/media/d3.73d51985.svg"
+  },
+  {
+    "revision": "a2de2317bdea4025ca0766292dda196f",
+    "url": "./static/media/death.a2de2317.svg"
+  },
+  {
+    "revision": "1aed6678237405492882a625cba337b9",
+    "url": "./static/media/fight.1aed6678.svg"
+  },
+  {
+    "revision": "61814f28bc6e10c30b32281cd36256c8",
+    "url": "./static/media/history.61814f28.jpg"
+  },
+  {
+    "revision": "3dab07689887cb1b426d13c463261cad",
+    "url": "./static/media/loving.3dab0768.svg"
+  },
+  {
+    "revision": "6fd55c70aba4c4e532dd2552284bfdc4",
+    "url": "./static/media/medical.6fd55c70.svg"
+  },
+  {
+    "revision": "b8a8d9030e277517ce74fa256e342063",
+    "url": "./static/media/race.b8a8d903.jpg"
+  },
+  {
+    "revision": "8e91accea61c2361089036d83636faf3",
+    "url": "./static/media/react.8e91acce.svg"
+  },
+  {
+    "revision": "136ce0f1e9d8973411695adcf6454cda",
+    "url": "./static/media/virus.136ce0f1.svg"
+  },
+  {
+    "revision": "6beba9447f363f89caed999ffc312663",
+    "url": "./static/media/world.6beba944.jpg"
+  }
+]);
